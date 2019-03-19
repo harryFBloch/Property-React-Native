e7c286f4a9730042faac5e4c4869337e045ed380
harryFBloch <harryfbloch@gmail.com>
2019-03-19 13:18:46 -0400

    worked on tutorial until i reached depricated code that no longer worked looking for a better one

--- App.js
+++ App.js
@@ -5,9 +5,11 @@
  * @format
  * @flow
  */
-
+'use strict'
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import SearchPage from './SearchPage'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,30 +22,23 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      // React.createElement(Text, {style: styles.description}, "Search for houses to buy!")
+      // <Text style={styles.description}>Search for Houses to buy!</Text>
+      <StackNavigator style={styles.container} initialRoute={{title: 'Property Finder', component: SearchPage}}/>
     );
   }
 }
 
+
+
 const styles = StyleSheet.create({
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 65
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
